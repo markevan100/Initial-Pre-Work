39cacadadae14b5bcd4cf5fcb06459a2e43f4ab7
Mark Mode <markevan100@gmail.com>
2019-06-30 08:49:41 -0600

    Find character at last index

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// Hmm, this is interesting. In Ruby, you can just do [-1]
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length-1];
