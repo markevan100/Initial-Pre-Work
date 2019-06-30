e5607acd0347ac4e0d450ce46fff86b2698ce6c7
Mark Mode <markevan100@gmail.com>
2019-06-30 13:24:24 -0600

    Add a second object to array

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,27 @@
+// Add a second object to array
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Sam",
+    "title": "Sam's Songs",
+    "release_year": 1993,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": false
+  }
+];
