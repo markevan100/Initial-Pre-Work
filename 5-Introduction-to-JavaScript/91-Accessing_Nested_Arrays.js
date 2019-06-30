5476b038cecbd6f7567708e151721f3e78a0af78
Mark Mode <markevan100@gmail.com>
2019-06-30 13:31:50 -0600

    Access trees in nested array

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+// This chain looks weird... but it works
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
