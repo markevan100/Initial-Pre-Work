c3c3a4f45e1725a0aecffcee6e4336989024b29c
Mark Mode <markevan100@gmail.com>
2019-06-30 08:23:08 -0600

    Create strings

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+// Doing this during the world cup, so...
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Tobin";
+var myLastName = "Heath";
