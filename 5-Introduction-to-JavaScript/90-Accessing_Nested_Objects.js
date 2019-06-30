36ea2a031e202fb5aa2b63024be8b5854b7ddfdd
Mark Mode <markevan100@gmail.com>
2019-06-30 13:28:48 -0600

    Chain dot and bracket calls on an object

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// Chaining dot and bracket calls on an object
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
