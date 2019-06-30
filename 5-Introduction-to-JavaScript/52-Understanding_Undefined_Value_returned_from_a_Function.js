cb5aa21d228454844e899e70bbcebe4666a04950
Mark Mode <markevan100@gmail.com>
2019-06-30 09:58:19 -0600

    Assign 2 to a variable

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+// Return undefined if you don't have a return statement
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum += 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
