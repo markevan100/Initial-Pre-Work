91a7053a9652ab17160c7a85f1e3cd8f67c3a955
Mark Mode <markevan100@gmail.com>
2019-06-30 11:50:36 -0600

    Build an object

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// Our first object
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Otto",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
