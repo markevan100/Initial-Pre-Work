f30b3f073b249fd3d07aa8a9ca70e31f1f75c4e9
Mark Mode <markevan100@gmail.com>
2019-06-30 08:37:27 -0600

    Concat with variables

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// Now it gets good, adding variables
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Mark";
+var myStr = "My name is " + myName + " and I am well!";
