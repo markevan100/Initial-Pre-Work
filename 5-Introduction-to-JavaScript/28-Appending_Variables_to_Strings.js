0e9ec73be6632b2d4a7eb4dda39c31b8c713f6b3
Mark Mode <markevan100@gmail.com>
2019-06-30 08:38:49 -0600

    Concat with variables and +=

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+// += with variables
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fun";
+var myStr = "Learning to code is ";
+myStr += someAdjective
