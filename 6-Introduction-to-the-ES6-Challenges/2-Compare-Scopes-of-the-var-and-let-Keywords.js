d67969ef594102940dae11ce2458f0dc5b0d51ab
Mark Mode <markevan100@gmail.com>
2019-07-01 12:51:36 -0600

    Update code with let

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+//Var is now const and let, so let is slightly different than var
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
