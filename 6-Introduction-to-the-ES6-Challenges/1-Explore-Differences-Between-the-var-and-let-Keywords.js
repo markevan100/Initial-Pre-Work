211b5962dd318b815ba4375ae68512936fba0afd
Mark Mode <markevan100@gmail.com>
2019-07-01 12:49:26 -0600

    Use let instead of var

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+//No one uses var anymore, now we're up to let
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
